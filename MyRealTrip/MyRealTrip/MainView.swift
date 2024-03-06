a186114994f223eff4b21b834e508e62823a488a
jjmmkk9 <abcdqwerty0192@gmail.com>
2024-03-06 17:54:36 +0900

    프로젝트 생성

---
+++ MyRealTrip/MyRealTrip/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  MyRealTrip
+//
+//  Created by 비긴어브랜드 on 3/6/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ MyRealTrip/MyRealTrip/MyRealTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRealTripApp.swift
+//  MyRealTrip
+//
+//  Created by 비긴어브랜드 on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRealTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
